80df02ef5b89ce6c636f4492ef57361a7e04a939
ArxLancifer <a_christoforidis@hotmail.com>
2022-10-16 20:53:04 +0300

    Expenses Tracker completed

--- src/compenents/Expenses/Expenses.js
+++ src/compenents/Expenses/Expenses.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpensesList from "./ExpensesList";
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
 
@@ -14,18 +15,18 @@ function Expenses(props) {
     return expense.date.getFullYear().toString() === filteredYear;
   })
 
-  let expensesContent = <p style={{color:"white"}}>No Expenses found.</p>
+//   let expensesContent = <p style={{color:"white"}}>No Expenses found.</p>
 
-  if(filteredExpenses.length > 0){
-    expensesContent = filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))
-  }
+//   if(filteredExpenses.length > 0){
+//     expensesContent = filteredExpenses.map((expense) => (
+//         <ExpenseItem
+//           key={expense.id}
+//           title={expense.title}
+//           amount={expense.amount}
+//           date={expense.date}
+//         />
+//       ))
+//   }
 
   return (
     <Card className="expenses">
@@ -33,7 +34,8 @@ function Expenses(props) {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {expensesContent}
+      <ExpensesChart expenses={filteredExpenses} />
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 }
