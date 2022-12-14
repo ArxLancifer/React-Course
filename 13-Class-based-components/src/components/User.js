cbc161196c67437bfaee85dcd8b5c86e9e59d3cc
ArxLancifer <a_christoforidis@hotmail.com>
2022-12-14 18:54:09 +0200

    error boundaries

--- 13-Class-based-components/src/components/User.js
+++ 13-Class-based-components/src/components/User.js
@@ -3,6 +3,11 @@ import classes from './User.module.css';
 
 //Class based Component
 class User extends Component{
+
+    componentWillUnmount(){
+        console.log('User will unmount') // Logged 3 times due we have 3 user names (components)
+    }
+
     render() {
         return <li className={classes.user}>{this.props.name}</li>;
     }
