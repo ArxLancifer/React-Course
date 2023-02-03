31373a25603d97f03de58d2e15f0ce9ae942bab6
ArxLancifer <a_christoforidis@hotmail.com>
2023-02-03 20:04:44 +0200

    custom hook

--- 15-Custom Hooks/onwards-to-a-more-realistic-example/src/hooks/use-http.js
+++ 15-Custom Hooks/onwards-to-a-more-realistic-example/src/hooks/use-http.js
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
-function useHttp(requestConfig, applyData){
+function useHttp(){
 
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
   
-    const sendRequest = async (taskText) => {
+    const sendRequest = useCallback(async (requestConfig, applyData) => {
       setIsLoading(true);
       setError(null);
       try {
@@ -28,7 +28,7 @@ function useHttp(requestConfig, applyData){
         setError(err.message || 'Something went wrong!');
       }
       setIsLoading(false);
-    };
+    }, []);
 
     return {
         isLoading: isLoading,
@@ -36,5 +36,6 @@ function useHttp(requestConfig, applyData){
         sendRequest: sendRequest
     };
 }
+
 
 export default useHttp;
